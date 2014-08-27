f690c1c5cb84f00c6ae4caecb0b5ca222909e73b
f-dz-v <f.dz.v.gh@gmail.com>
2014-08-27 14:23:46 +0300

    + TQueue, TBQueue

---
+++ src/TBQueue.swift
@@ -0,0 +1,110 @@
+// Copyright (c) 2014, f-dz-v
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+public class TBQueue<T: Equatable> {
+    private var _q:TVarArray<T> = newTVar([])
+    private var _size:TVar<Int>
+    private init(_ size: Int) {
+        self._size = newTVar(size)
+    }
+}
+
+public func newTBQueue<T> (size: Int) -> TBQueue<T> {
+    return TBQueue(size)
+}
+
+public func newTBQueueSTM<T> (size: Int) -> STM<TBQueue<T>> {
+    return returnM(TBQueue(size))
+}
+
+public func writeTBQueue<T> (queue: TBQueue<T>, val: T) -> STM<()> {
+    return ( readTVar(queue._size) >>>= {(size) -> STM<()> in
+             if size == 0 {
+                return retry()
+             } else {
+                return writeTVar(queue._size, size - 1) >>>
+                     { modifyTVar(queue._q, {$0 + [val]}) }
+             }} )
+}
+
+public func readTBQueue<T> (queue: TBQueue<T>) -> STM<T> {
+    return ( readTVar(queue._q) >>>= { xs in
+        if xs.isEmpty {
+            return retry()
+        }
+        let res = xs[0]
+        var newArr = xs
+        newArr.removeAtIndex(0)
+        return writeTVar(queue._q, newArr) >>>
+             { modifyTVar(queue._size, {$0+1}) } >>>
+             { returnM(res)}
+        } )
+}
+
+// TODO: orElse
+public func tryReadTBQueue<T> (queue: TBQueue<T>) -> STM<T?> {
+    return ( readTVar(queue._q) >>>= { xs in
+        if xs.isEmpty {
+            return returnM(nil)
+        }
+        let res = xs[0]
+        var newArr = xs
+        newArr.removeAtIndex(0)
+        return writeTVar(queue._q, newArr) >>>
+            { modifyTVar(queue._size, {$0+1}) } >>>
+            { returnM(res)}
+        } )
+}
+
+public func unGetTBQueue<T> (queue: TBQueue<T>, val: T) -> STM<()> {
+    return readTVar(queue._size) >>>= { size in
+        if size == 0 {
+            return retry()
+        } else {
+            return modifyTVar(queue._q, {[val] + $0}) >>>
+                 { modifyTVar(queue._size, {$0-1}) }
+        }
+    }
+}
+
+public func peekTBQueue<T> (queue: TBQueue<T>) -> STM<T> {
+    return ( readTBQueue(queue) >>>= { x in
+             unGetTBQueue(queue, x) >>> {
+             returnM(x)
+           }})
+}
+
+public func tryPeekTBQueue<T> (queue: TBQueue<T>) -> STM<T?> {
+    return ( tryReadTBQueue(queue) >>>= { x in
+        if let _x = x {
+            return unGetTBQueue(queue, _x) >>> { returnM(_x) }
+        } else {
+            return returnM(nil)
+        }
+        })
+}
+
+public func isEmptyTBQueue<T> (queue: TBQueue<T>) -> STM<Bool> {
+    return ( readTVar(queue._q) >>>= { returnM($0.isEmpty)} )
+}
---
+++ src/TQueue.swift
@@ -0,0 +1,89 @@
+// Copyright (c) 2014, f-dz-v
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+public class TQueue<T: Equatable> {
+    private var _q:TVarArray<T> = newTVar([])
+}
+
+public func newTQueue<T> () -> TQueue<T> {
+    return TQueue()
+}
+
+public func newTQueueSTM<T> () -> STM<TQueue<T>> {
+    return returnM(TQueue())
+}
+
+public func writeTQueue<T> (queue: TQueue<T>, val: T) -> STM<()> {
+    return modifyTVar(queue._q, {$0 + [val]})
+}
+
+public func readTQueue<T> (queue: TQueue<T>) -> STM<T> {
+    return ( readTVar(queue._q) >>>= { xs in
+        if xs.isEmpty {
+            return retry()
+        }
+        let res = xs[0]
+        var newArr = xs
+        newArr.removeAtIndex(0)
+        return (writeTVar(queue._q, newArr) >>> {returnM(res)})
+        } )
+}
+
+// TODO: orElse
+public func tryReadTQueue<T> (queue: TQueue<T>) -> STM<T?> {
+    return ( readTVar(queue._q) >>>= { xs in
+        if xs.isEmpty {
+            return returnM(nil)
+        }
+        let res = xs[0]
+        var newArr = xs
+        newArr.removeAtIndex(0)
+        return writeTVar(queue._q, newArr) >>> {returnM(res)}
+        } )
+}
+
+public func unGetTQueue<T> (queue: TQueue<T>, val: T) -> STM<()> {
+    return modifyTVar(queue._q, {[val] + $0})
+}
+
+public func peekTQueue<T> (queue: TQueue<T>) -> STM<T> {
+    return ( readTQueue(queue) >>>= { x in
+             unGetTQueue(queue, x) >>> {
+             returnM(x)
+           }})
+}
+
+public func tryPeekTQueue<T> (queue: TQueue<T>) -> STM<T?> {
+    return ( tryReadTQueue(queue) >>>= { x in
+             if let _x = x {
+                return unGetTQueue(queue, _x) >>> { returnM(_x) }
+             } else {
+                return returnM(nil)
+             }
+           })
+}
+
+public func isEmptyTQueue<T> (queue: TQueue<T>) -> STM<Bool> {
+    return ( readTVar(queue._q) >>>= { returnM($0.isEmpty)} )
+}
